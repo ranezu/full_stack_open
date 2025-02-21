c15ecb9430b170869d7831b6b9fe4159286a373d
ranezu <sami.m.rantala@gmail.com>
2025-02-21 13:44:35 +0000

    feat: add changes for exercise 1.4

--- part_1/1.2/src/App.jsx
+++ part_1/1.2/src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => (
     <h1>{props.name}</h1>
@@ -26,9 +28,9 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part name={part1.name} exercises={part1.exercises} />
-        <Part name={part2.name} exercises={part2.exercises} />
-        <Part name={part3.name} exercises={part3.exercises} />
+        <Part name={parts[0].name} exercises={parts[0].exercises} />
+        <Part name={parts[1].name} exercises={parts[1].exercises} />
+        <Part name={parts[2].name} exercises={parts[2].exercises} />
       </div>
     )
   }
@@ -43,7 +45,7 @@ const App = () => {
 
       <Content />
 
-      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
+      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
