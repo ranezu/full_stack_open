3e8b0327c63a8b2fc093b4d7d9fa680230331952
ranezu <sami.m.rantala@gmail.com>
2025-02-21 13:41:51 +0000

    feat: changes for exercise 1.3

--- part_1/1.2/src/App.jsx
+++ part_1/1.2/src/App.jsx
@@ -1,11 +1,17 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => (
     <h1>{props.name}</h1>
@@ -20,9 +26,9 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part name={part1} exercises={exercises1} />
-        <Part name={part2} exercises={exercises2} />
-        <Part name={part3} exercises={exercises3} />
+        <Part name={part1.name} exercises={part1.exercises} />
+        <Part name={part2.name} exercises={part2.exercises} />
+        <Part name={part3.name} exercises={part3.exercises} />
       </div>
     )
   }
@@ -37,7 +43,7 @@ const App = () => {
 
       <Content />
 
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
