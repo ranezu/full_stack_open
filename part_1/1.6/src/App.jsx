2cacead5c6a0830c5adb2e88ef3456e0b291eb1e
ranezu <sami.m.rantala@gmail.com>
2025-02-28 13:27:54 +0000

    feat: add 1.6

---
+++ part_1/1.6/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={() => props.setter(props.oldValue + 1)}>{props.text}</button>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button setter={setGood} oldValue={good} text='good' />
+      <Button setter={setNeutral} oldValue={neutral} text='neutral' />
+      <Button setter={setBad} oldValue={bad} text='bad' />
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
